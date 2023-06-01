4911a4ff42d8ab0193079412fede93dc9101115e
Dell <yiyichanmyaewinshein@robust.best>
2023-06-02 01:14:57 +0630

    metric changing to test_rmse

--- homework/homework2/register_model.py
+++ homework/homework2/register_model.py
@@ -74,7 +74,7 @@ def run_register_model(data_path: str, top_n: int):
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
-        order_by=["metrics.rmse ASC"]
+        order_by=["metrics.test_rmse ASC"]
     )[0]
 
     # Register the best model
